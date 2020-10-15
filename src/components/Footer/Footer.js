2664fa14dbd078e0b10a85f54218cc5be16f5673
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-15 18:33:43 +0700

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,53 @@
 import React from "react";
 import styles from "./Footer.module.css";
 
+import logo from "../../assets/general/Logo.svg";
+
 const Footer = () => {
-  return <div className={styles.container}>This is a footer</div>;
+  return (
+    <>
+      <div className={styles.borderBottom}>
+        <div className={styles.container}>
+          <div className={styles.footerContainer}>
+            <img src={logo} alt="logo" className={styles.logo} />
+            <div className={styles.footerLinkContainer}>
+              <div className={styles.footerLink}>
+                <h4>Fundraise for</h4>
+                <ul>
+                  <li>Education</li>
+                  <li>Disabilities</li>
+                  <li>Hospital Bills</li>
+                  <li>Religious</li>
+                </ul>
+              </div>
+              <div className={styles.footerLink}>
+                <h4>Learn More</h4>
+                <ul>
+                  <li>How we can help</li>
+                  <li>FAQ</li>
+                  <li>Blog</li>
+                </ul>
+              </div>
+              <div className={styles.footerLink}>
+                <h4>Resources</h4>
+                <ul>
+                  <li>Contact Us</li>
+                  <li>About</li>
+                  <li>Careers</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <p className={styles.copyright}>
+          TaliKasih &#169; 2020. All rights reserved
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
