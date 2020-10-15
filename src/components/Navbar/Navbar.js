d648ce1e5f3938724df2ec9248d2127c258f1dc3
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-15 21:22:25 +0700

    finish navbar styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,32 +5,51 @@ import search from "../../assets/general/search-icon.svg";
 
 const Navbar = () => {
   const [openSearch, setOpenSearch] = useState(false);
+  const [createBtn, setCreateBtn] = useState(true);
+
+  const handleOpenSearch = () => {
+    setOpenSearch(true);
+    setCreateBtn(false);
+  };
+
+  const handleCloseSearch = () => {
+    setOpenSearch(false);
+    setTimeout(() => {
+      setCreateBtn(true);
+    }, 500);
+  };
 
   return (
     <div className={styles.navContainer}>
       <div className={styles.container}>
         <img src={logo} alt="logo" className={styles.logo} />
         <div className={styles.navLink}>
-          <span
-            className={`${styles.createCampaignBtn} ${
-              !openSearch && styles.openBtn
-            }`}
-          >
-            Create Campaign
-          </span>
-          <span
-            className={`${styles.donateBtn} ${!openSearch && styles.openBtn}`}
-          >
-            Donate
-          </span>
+          {createBtn && (
+            <>
+              <span
+                className={`${styles.createCampaignBtn} ${
+                  !openSearch && styles.openBtn
+                }`}
+              >
+                Create Campaign
+              </span>
+              <span
+                className={`${styles.donateBtn} ${
+                  !openSearch && styles.openBtn
+                }`}
+              >
+                Donate
+              </span>
+            </>
+          )}
           <div className={styles.searchContainer}>
             <img src={search} alt="search icon" className={styles.searchIcon} />
             <input
               className={`${styles.searchInput} ${openSearch && styles.open}`}
               type="text"
               placeholder="Search"
-              onMouseEnter={() => setOpenSearch(true)}
-              onMouseLeave={() => setOpenSearch(false)}
+              onClick={handleOpenSearch}
+              onBlur={handleCloseSearch}
             />
           </div>
           <span className={styles.loginBtn}>Login</span>
