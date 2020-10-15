63d925058b8e6f2754ed569680089c45d858696e
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-15 16:00:01 +0700

    Campaign Card : Components and styling

--- src/components/Home/NewCampaign/NewCampaign.js
+++ src/components/Home/NewCampaign/NewCampaign.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CampaignCard from "../../CampaignCard/CampaignCard";
 import styles from "./NewCampaign.module.css";
 
 const NewCampaign = () => {
@@ -8,6 +9,11 @@ const NewCampaign = () => {
       <h2 className={styles.topicTitle}>
         The latest people who need your help
       </h2>
+      <div className={styles.cardsContainer}>
+        <CampaignCard />
+        <CampaignCard />
+        <CampaignCard />
+      </div>
     </div>
   );
 };
