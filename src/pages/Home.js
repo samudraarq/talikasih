1819bdee239af0c4e8f4611a1a52ce20f30f29c3
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-15 19:02:41 +0700

    fix styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,15 @@ import Trending from "../components/Home/Trending/Trending";
 const Home = () => {
   return (
     <>
-      <Trending />
-      <NewCampaign />
-      <DownloadApps />
+      <div
+        style={{
+          background: "linear-gradient(#ffffff 0%, #ffffff 50%, #f2f9fa 100%)",
+        }}
+      >
+        <Trending />
+        <NewCampaign />
+        <DownloadApps />
+      </div>
       <Footer />
     </>
   );
