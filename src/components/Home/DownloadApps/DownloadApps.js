97d5305222ee440944df61437cdddd19d624349f
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-15 17:06:16 +0700

    download segment

--- src/components/Home/DownloadApps/DownloadApps.js
+++ src/components/Home/DownloadApps/DownloadApps.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./DownloadApps.module.css";
 
+import playStore from "../../../assets/homepage/Home/google-play-badge.png";
+import appleStore from "../../../assets/homepage/Home/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg";
+
 import peopleRunning from "../../../assets/homepage/Home/Rectangle 38.png";
 
 const DownloadApps = () => {
@@ -13,9 +16,16 @@ const DownloadApps = () => {
           alt="people running"
           className={styles.bigImage}
         />
-        <div className={styles.downloadBtnContainer}>
+        <div>
           <p className={styles.available}>Available Now</p>
-          <div className={styles.btnContainer}></div>
+          <div className={styles.btnContainer}>
+            <img src={appleStore} alt="App Store" className={styles.appBadge} />
+            <img
+              src={playStore}
+              alt="Play Store"
+              className={styles.playBadge}
+            />
+          </div>
         </div>
       </div>
     </div>
