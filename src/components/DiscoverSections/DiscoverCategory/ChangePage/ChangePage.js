697e5198b3e9958491955f6c41531f35577a15b9
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-16 15:06:17 +0700

    Add Change Page components

---
+++ src/components/DiscoverSections/DiscoverCategory/ChangePage/ChangePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ChangePage.module.css";
+
+const ChangePage = () => {
+  return <div>for pagination</div>;
+};
+
+export default ChangePage;
--- src/components/DiscoverSections/DiscoverCategory/DiscoverCategory.js
+++ src/components/DiscoverSections/DiscoverCategory/DiscoverCategory.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ChangePage from "./ChangePage/ChangePage";
 import DiscoverCards from "./DiscoverCards/DiscoverCards";
 import styles from "./DiscoverCategory.module.css";
 import HeaderCategory from "./HeaderCategory/HeaderCategory";
@@ -12,6 +13,7 @@ const DiscoverCategory = () => {
       <div className={styles.campaignContainer}>
         <SortButton />
         <DiscoverCards />
+        <ChangePage />
       </div>
     </div>
   );
