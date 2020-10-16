9a9c93c48cc6d6c41c8171e60ae20f3c87be2aa2
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-16 14:08:43 +0700

    Sort button

--- src/components/DiscoverSections/DiscoverCategory/SortButton/SortButton.js
+++ src/components/DiscoverSections/DiscoverCategory/SortButton/SortButton.js
@@ -1,14 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import queryString from "query-string";
 import styles from "./SortButton.module.css";
 
 import sort from "../../../../assets/discover/sort.svg";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 const SortButton = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const { category } = useParams();
+  const location = useLocation();
+
+  useEffect(() => {
+    const parsed = queryString.parse(location.search);
+    console.log(parsed);
+  }, [location]);
+
+  const handleClick = () => {
+    setOpenMenu(true);
+
+    document.addEventListener("click", handleClose);
+  };
+
+  const handleClose = () => {
+    setOpenMenu(false);
+
+    document.removeEventListener("click", handleClose);
+  };
+
   return (
-    <div className={styles.sortContainer}>
-      <p className={styles.sortText}>Sort</p>
-      <img src={sort} alt="sort" className={styles.sortIcon} />
-    </div>
+    <>
+      <div className={styles.sortContainer}>
+        <div onClick={handleClick} className={styles.sortIconContainer}>
+          <p className={styles.sortText}>Sort</p>
+          <img src={sort} alt="sort" className={styles.sortIcon} />
+        </div>
+
+        {openMenu && (
+          <div className={styles.menuContainer}>
+            <Link
+              to={`/discover/${category}?sort=newest`}
+              className={styles.menuItem}
+              onClick={handleClose}
+            >
+              Newest
+            </Link>
+            <Link
+              to={`/discover/${category}?sort=mosturgent`}
+              className={styles.menuItem}
+              onClick={handleClose}
+            >
+              Most Urgent
+            </Link>
+            <Link
+              to={`/discover/${category}?sort=popular`}
+              className={styles.menuItem}
+              onClick={handleClose}
+            >
+              Popular
+            </Link>
+            <Link
+              to={`/discover/${category}?sort=lessdonation`}
+              className={styles.menuItem}
+              onClick={handleClose}
+            >
+              Less Donation
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
