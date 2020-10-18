db9f00e6106c2bf4cf7e7e3f00d1c8905530a807
blackwolfinc <inc.blackwolf@gmail.com>
2020-10-18 23:05:15 +0700

    reed more the story done

--- src/components/CampaingDetailsDonate/CampaingDetailsDonate.js
+++ src/components/CampaingDetailsDonate/CampaingDetailsDonate.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { CampaingDetailsDonateBigCard } from './CampaingDetailsDonateBigCard/CampaingDetailsDonateBigCard'
+import CampaingDetailsDonateTheStory from './CampaingDetailsDonateTheStory/CampaingDetailsDonateTheStory'
 
  const CampaingDetailsDonate = () => {
     return (
         <div>
             <CampaingDetailsDonateBigCard/>
-            
+            <CampaingDetailsDonateTheStory/>
         </div>
     )
 }
