d73ad4725e1439ee7d5c14e6583bb2a8c04633b3
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-18 13:09:21 +0700

    add my profile components

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,8 +1,13 @@
 import React from "react";
+import MyProfile from "./MyProfile/MyProfile";
 import styles from "./UserProfile.module.css";
 
 const UserProfile = () => {
-  return <div className={styles.container}>From user profile</div>;
+  return (
+    <div className={styles.container}>
+      <MyProfile />
+    </div>
+  );
 };
 
 export default UserProfile;
