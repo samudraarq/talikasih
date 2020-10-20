d845f531cfafa6eb5dfc786182fa718c5f0305c4
blackwolfinc <inc.blackwolf@gmail.com>
2020-10-20 17:22:35 +0700

    mas sam

--- src/components/CampaignDetailsDonate/CampaignDetailsDonate.js
+++ src/components/CampaignDetailsDonate/CampaignDetailsDonate.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState,useEffect } from "react";
 import CampaignDetailsDonateAllComent from './CampaignDetailsDonateAllComent/CampaignDetailsDonateAllComent';
 import { CampaignDetailsDonateBigCard } from './CampaignDetailsDonateBigCard/CampaignDetailsDonateBigCard';
 import { CampaignDetailsDonateComentFrom } from './CampaignDetailsDonateComentFrom/CampaignDetailsDonateComentFrom';
@@ -6,9 +6,27 @@ import { CampaignDetailsDonateDonation } from './CampaignDetailsDonateDonation/C
 import CampaignDetailsDonateRelatedCampaing from './CampaignDetailsDonateRelatedCampaing/CampaignDetailsDonateRelatedCampaing';
 import CampaignDetailsDonateTheStory from './CampaignDetailsDonateTheStory/CampaignDetailsDonateTheStory';
 import CampaignDetailsDonateUpdates from './CampaignDetailsDonateUpdates/CampaignDetailsDonateUpdates';
-
+import axios from 'axios'
 
  const CampaignDetailsDonate = () => {
+    const [DataDonate, setDataDonate] = useState([]);
+    useEffect(() => {
+      
+
+        axios('https://warm-tundra-23736.herokuapp.com/campaign/1')
+        .then(function (response) {
+            console.log(response.data)
+            setDataDonate(response.data)
+            
+
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+
+        
+      }, []);
+
     return (
         <div>
             <CampaignDetailsDonateBigCard/>
