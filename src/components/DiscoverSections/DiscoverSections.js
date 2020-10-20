3faf96037ed13e2f40eb83fe2648cbc6c6952534
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-20 17:20:25 +0700

    add reducers

--- src/components/DiscoverSections/DiscoverSections.js
+++ src/components/DiscoverSections/DiscoverSections.js
@@ -1,36 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import {
+  getNewCampaigns,
+  getPopularCampaigns,
+} from "../../redux/actions/campaignActions";
 import styles from "./DiscoverSections.module.css";
 import CampaignCard from "../CampaignCard/CampaignCard";
 
-const DiscoverSections = () => {
+const DiscoverSections = ({
+  newCampaign,
+  popularCampaign,
+  getNewCampaigns,
+  getPopularCampaigns,
+}) => {
+  useEffect(() => {
+    getNewCampaigns(1);
+    getPopularCampaigns(1);
+  }, [getNewCampaigns, getPopularCampaigns]);
+
+  const renderNewCampaigns = newCampaign?.documents
+    .slice(0, 3)
+    .map((campaign) => <CampaignCard campaign={campaign} key={campaign.id} />);
+
+  const renderPopularCampaigns = popularCampaign?.documents
+    .slice(0, 3)
+    .map((campaign) => <CampaignCard campaign={campaign} key={campaign.id} />);
+
   return (
     <div className={styles.container}>
       <div className={styles.sectionsContainer}>
         <span className={styles.sectionsTitle}>Newest</span>
-        <div className={styles.cardsContainer}>
-          <CampaignCard />
-          <CampaignCard />
-          <CampaignCard />
-        </div>
+        <div className={styles.cardsContainer}>{renderNewCampaigns}</div>
       </div>
       <div className={styles.sectionsContainer}>
         <span className={styles.sectionsTitle}>Most Urgent</span>
         <div className={styles.cardsContainer}>
+          {/* <CampaignCard />
           <CampaignCard />
-          <CampaignCard />
-          <CampaignCard />
+          <CampaignCard /> */}
         </div>
       </div>
       <div className={styles.sectionsContainer}>
         <span className={styles.sectionsTitle}>Gained Momentum</span>
-        <div className={styles.cardsContainer}>
-          <CampaignCard />
-          <CampaignCard />
-          <CampaignCard />
-        </div>
+        <div className={styles.cardsContainer}>{renderPopularCampaigns}</div>
       </div>
     </div>
   );
 };
 
-export default DiscoverSections;
+const mapStateToProps = (state) => {
+  return {
+    newCampaign: state.newCampaign,
+    popularCampaign: state.popularCampaign,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getNewCampaigns: (page) => dispatch(getNewCampaigns(page)),
+    getPopularCampaigns: (page) => dispatch(getPopularCampaigns(page)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DiscoverSections);
