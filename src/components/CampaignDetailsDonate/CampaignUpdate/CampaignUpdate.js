038060b429a5bcaf31c5da78d74ab36bd15306ba
gilbert yohanes <yohanesbert97@gmail.com>
2020-11-02 21:02:47 +0700

    revise api

--- src/components/CampaignDetailsDonate/CampaignUpdate/CampaignUpdate.js
+++ src/components/CampaignDetailsDonate/CampaignUpdate/CampaignUpdate.js
@@ -6,25 +6,26 @@ import { connect } from "react-redux";
 import qs from "qs";
 import "quill/dist/quill.snow.css";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import close from "../../../assets/CampingCreate/close.png";
 
 function CampaignUpdate({ auth, requestClose }) {
   // FORM //
   const [openAmount, setOpenAmount] = useState(false);
   const { register, handleSubmit, errors } = useForm();
+  let history = useHistory();
 
   const onSubmit = async (data) => {
-    // const inputText = quill.getContents();
-    const inputText = quill.getText();
-    // console.log(quill.getContents());
-    // console.log(data);
-    // console.log(inputText);
+    const inputText = quill.root.innerHTML;
+    const dateToday = new Date();
+    const campaignId = 1;
     try {
       const { ammount } = data;
       const updateInfo = qs.stringify({
         ammount,
         content: inputText,
+        date: dateToday,
+        StatusId: campaignId,
       });
       // console.log(updateInfo);
       const response = await axios({
@@ -32,15 +33,16 @@ function CampaignUpdate({ auth, requestClose }) {
         url: "https://warm-tundra-23736.herokuapp.com/campaignLog/1",
         data: updateInfo,
         headers: {
-          // token:
-          //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwibmFtZSI6ImpvaG4iLCJyb2xlIjoidXNlciIsImlhdCI6MTYwMzE5MzI5OX0.DqCMxWap7-rM7AdgRVo2yZnqDapQNjqG0aTo9s7v7d4",
-          token: auth.token,
+          token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwibmFtZSI6ImpvaG4iLCJyb2xlIjoidXNlciIsImlhdCI6MTYwMzE5MzI5OX0.DqCMxWap7-rM7AdgRVo2yZnqDapQNjqG0aTo9s7v7d4",
+          // token: auth.token,
         },
       });
       console.log(response.data);
     } catch (error) {
       console.log(error, "error");
     }
+    history.push("/user/profile");
   };
 
   // Editor //
@@ -125,9 +127,7 @@ function CampaignUpdate({ auth, requestClose }) {
             }}
           />
           <div className={styles.sumbitBtn}>
-            <Link to="/user/profile">
-              <input type="submit" value="submit" className={styles.sumbit} />
-            </Link>
+            <input type="submit" value="submit" className={styles.sumbit} />
           </div>
         </div>
       </form>
