7773f9e5deec2519e5dbe49bd2736149775e0cbe
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-03 16:05:15 +0700

    change redux store

--- src/redux/store/combinedStore.js
+++ src/redux/store/combinedStore.js
@@ -2,10 +2,10 @@ import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducer from "../reducers/index";
 
-const allEnhancers = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
 );
 
-const store = createStore(reducer, allEnhancers);
 export default store;
