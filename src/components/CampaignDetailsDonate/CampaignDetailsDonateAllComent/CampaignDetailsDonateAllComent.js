d80e3277dcea47adb71afbb8b14710ebdfab48eb
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-12 10:31:32 +0700

    fix font size and remove reverse

--- src/components/CampaignDetailsDonate/CampaignDetailsDonateAllComent/CampaignDetailsDonateAllComent.js
+++ src/components/CampaignDetailsDonate/CampaignDetailsDonateAllComent/CampaignDetailsDonateAllComent.js
@@ -2,32 +2,34 @@ import React from "react";
 import styles from "./CampaignDetailsDonateAllComent.module.css";
 import cardImg1 from "../../../assets/general/default_image.jpeg";
 import { connect } from "react-redux";
-import moment from 'moment'
-import {useState} from 'react'
+import moment from "moment";
+import { useState } from "react";
 
 const CampaignDetailsDonateAllComent = (dataDonorAll) => {
-  const [limit, setLimit ] = useState(2);
-  const [loadMoreBol, setloadMoreBol ] = useState(true);
-  const loadLess = ()=>{
+  const [limit, setLimit] = useState(2);
+  const [loadMoreBol, setloadMoreBol] = useState(true);
+  const loadLess = () => {
     setLimit(2);
     setloadMoreBol(true);
-  }
-  const loadMore = () =>{
+  };
+  const loadMore = () => {
     setLimit();
-    setloadMoreBol(false) ;
-  }
+    setloadMoreBol(false);
+  };
 
-  
-  const cekisi =dataDonorAll.dataDonorAll?.dataDonateComen.length;
+  const cekisi = dataDonorAll.dataDonorAll?.dataDonateComen.length;
 
-;  const UserAllComent = dataDonorAll.dataDonorAll?.dataDonateComen?.slice(0, limit).map(
-    (item, index) => {
+  const UserAllComent = dataDonorAll.dataDonorAll?.dataDonateComen
+    ?.slice(0, limit)
+    .map((item, index) => {
       return (
-
         <div className={styles.card} key={index}>
           <div className={styles.cardheader}>
             <div className={styles.foto}>
-              <img src={item?.User?.photo?   item?.User?.photo  : cardImg1 } alt='user'></img>
+              <img
+                src={item?.User?.photo ? item?.User?.photo : cardImg1}
+                alt="user"
+              ></img>
             </div>
             <div className={styles.info}>
               <h5>{item?.User?.name}</h5>
@@ -35,29 +37,33 @@ const CampaignDetailsDonateAllComent = (dataDonorAll) => {
             </div>
           </div>
           <div className={styles.cardbody}>
-          {item?.content ? item?.content:"Komentar Kosong"}
+            {item?.content ? item?.content : "Komentar Kosong"}
           </div>
         </div>
-       
       );
-    }
-  );
+    });
   return (
     <div className={styles.body}>
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-    
-       {UserAllComent}
-        {/* loping end */}
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          {UserAllComent}
+          {/* loping end */}
+        </div>
+        <div className={styles.btnwrap}>
+          {/* button di taruh fungsi untuk menganti max slice */}
+          {cekisi === 0 ? (
+            <h1>No Comment</h1>
+          ) : loadMoreBol ? (
+            <button onClick={loadMore} className={styles.btnLoadmore}>
+              LOAD MORE
+            </button>
+          ) : (
+            <button onClick={loadLess} className={styles.btnLoadmore}>
+              SHOW LESS
+            </button>
+          )}
+        </div>
       </div>
-      <div className={styles.btnwrap}>
-        {/* button di taruh fungsi untuk menganti max slice */}
-            {cekisi ===0  ? <h1>No Comment</h1> : 
-            loadMoreBol  ? <button onClick={loadMore}  className={styles.btnLoadmore}>LOAD MORE</button>  : <button onClick={loadLess}  className={styles.btnLoadmore}>SHOW LESS</button> } 
-            
-
-      </div>
-    </div>
     </div>
   );
 };
