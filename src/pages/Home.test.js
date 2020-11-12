9a05b4f20eb19db6cdb50849d82964e2415d2674
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-11-12 17:08:34 +0700

    Home page test

---
+++ src/pages/Home.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import Home from "./Home";
+import Navbar from "../components/Navbar/Navbar";
+import Carousel from "../components/Carousel/Carousel";
+import Trending from "../components/Home/Trending/Trending";
+import NewCampaign from "../components/Home/NewCampaign/NewCampaign";
+import DownloadApps from "../components/Home/DownloadApps/DownloadApps";
+
+configure({ adapter: new Adapter() });
+
+describe("<Home />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Home />);
+  });
+
+  it("should render one <Carousel />", () => {
+    expect(wrapper.find(Carousel)).toHaveLength(1);
+  });
+  it("should render one <Navbar />", () => {
+    expect(wrapper.find(Navbar)).toHaveLength(1);
+  });
+  it("should render one <Trending />", () => {
+    expect(wrapper.find(Trending)).toHaveLength(1);
+  });
+  it("should render one <NewCampaign />", () => {
+    expect(wrapper.find(NewCampaign)).toHaveLength(1);
+  });
+  it("should render one <DownloadApps />", () => {
+    expect(wrapper.find(DownloadApps)).toHaveLength(1);
+  });
+});
